476b7aa5556c728ef852afca327cf3126a7bf669
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2022-11-28 21:16:41 +0200

    new bassant

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -55,21 +55,7 @@ export const Navbar = () => {
             </div>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item dropdown">
-                <Dropdown>
-                  <Dropdown.Toggle id="dropdown-basic" className="boooootstrap">
-                    {t("header.home")}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu className="dropdown-menu">
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">
-                      Another action
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
-                      Something else
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </li>
+                
               <li className="nav-item dropdown">
                 <Link
                   className="nav-link dropdown-toggle header-items"

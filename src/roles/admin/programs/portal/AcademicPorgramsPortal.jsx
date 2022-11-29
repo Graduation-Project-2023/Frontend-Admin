eaca5ab07e9930258b36406337dac06ee4df591f
DoniaAboelyazed <doniaaboelyazed12@gmail.com>
2022-11-29 13:30:32 +0200

    test2

--- src/roles/admin/programs/portal/AcademicPorgramsPortal.jsx
+++ src/roles/admin/programs/portal/AcademicPorgramsPortal.jsx
@@ -3,7 +3,7 @@ import styles from "./AcademicPorgramsPortal.module.scss";
 export const AcademicPorgramsPortal = () => {
   return (
     <div className="container">
-      <div></div>
+      <div>doniaaaaaaaaaaaaaaaaaaaaaaaa</div>
     </div>
   );
 };

e9116e5466e122e4f251a8bbf693bdf75a65f018
DoniaAboelyazed <doniaaboelyazed12@gmail.com>
2022-11-29 17:03:42 +0200

    academicprogramportal

--- src/roles/admin/programs/portal/AcademicPorgramsPortal.jsx
+++ src/roles/admin/programs/portal/AcademicPorgramsPortal.jsx
@@ -2,8 +2,27 @@ import styles from "./AcademicPorgramsPortal.module.scss";
 
 export const AcademicPorgramsPortal = () => {
   return (
-    <div className="container">
-      <div>doniaaaaaaaaaaaaaaaaaaaaaaaa</div>
-    </div>
+ <div className="container">
+  <div className={styles.portal_body}>
+      <h5 className={styles.portal_title}>البرامج الدراسية</h5>
+
+          <div class={styles.portal_search}>
+                    <input type="text" class={styles.portal_search_rec} placeholder="اسم البرنامج.."/> 
+          </div>
+
+       <div className={styles.portal_list}>
+          <li >اضافة برامج جديدة <button className={styles.portal_btn}> + </button></li>
+          <li >المستوى الاول</li>
+          <li >المستوى الاول لائحة </li>
+          <li>الهندسة الكهربية</li>
+          <li>الهندسة الكهربية شعبة..</li>
+          <li>الهندسة الميكانيكية</li>
+          <li >الهندسة الميكانيكية شعية..</li>
+          <li >الهندسة المدنية</li>
+          <li > الهندسة المدنية شعبة..</li>
+          <li >الهندسة المعمارية</li>
+       </div>
+       </div>
+  </div>
   );
 };

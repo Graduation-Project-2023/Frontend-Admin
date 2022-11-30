0cc0f750158e88dd4d26e7f1358348f393ad17b7
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2022-11-30 15:39:12 +0200

    table&allowed hours

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,48 @@
 import { useTranslation } from "react-i18next";
+import { CiEdit } from "react-icons/ci";
+import { RiDeleteBackLine } from "react-icons/ri";
 
 export const Table = (props) => {
+  const tabletitle=props.tabletitle;
   const headerItems = props.headerItems;
   const rowItems = props.rowItems;
   const editableItems = props.editableItems;
   const deletableItems = props.deletableItems;
+  const { t } = useTranslation();
+  
 
   return (
-    <table>
+    <div className="tablecard">
+    <table className="table tbl">
+
       <thead>
+
         <tr>
           {headerItems.map((item) => {
-            return <th key={item.id}>{item.title}</th>;
+            return <th key={item.id} className="tbl-header">{item.title}</th>;
           })}
-          {editableItems && <th>Edit</th>}
-          {deletableItems && <th>Delete</th>}
+          {editableItems && <th className="tbl-header">{t(`common.edit`)}</th>}
+          {deletableItems && <th className="tbl-header" >{t(`common.delete`)}</th>}
         </tr>
       </thead>
+      
       <tbody>
         {rowItems.map((item) => {
           return (
             <tr key={item.id}>
-              <td>{item.title}</td>
-              <td>{item.level}</td>
-              <td>{item.minHours}</td>
-              <td>{item.maxHours}</td>
+              <td className="tbl-items">{item.title}</td>
+              <td className="tbl-items">{item.level}</td>
+              <td className="tbl-items">{item.minHours}</td>
+              <td className="tbl-items">{item.maxHours}</td>
+              {editableItems && <td className="tbl-items"><CiEdit color="#858D97"fontSize="1.5rem" /></td>}
+              {deletableItems && <td className="tbl-items"><RiDeleteBackLine color="#D65050" fontSize="1.5rem"/></td>}
             </tr>
           );
         })}
         
       </tbody>
+      
     </table>
+    </div>
   );
 };
--- src/roles/admin/programs/level-allowed-hours/LevelHours.jsx
+++ src/roles/admin/programs/level-allowed-hours/LevelHours.jsx
@@ -1,26 +1,118 @@
 import { Table } from "../../../../components/Table";
 import styles from "./LevelHours.module.scss";
+import { useTranslation } from "react-i18next";
+import {Link} from "react-router-dom"
 
 export const LevelHours = () => {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      hello
+    <div className="container">
+      <div className={styles.formcard}>
+
+      <h3>{t(`levelHours.formhead`)}</h3>
+      <span>  <Link >{t(`adminNavbar.academic`)}</Link></span>
+      <span> 	&gt; <Link >{t(`common.level1`)}</Link></span>
+      <span> 	&gt; <Link >{t(`levelHours.formhead`)}</Link></span>
+      <form >
+        <div className="row mb-4">
+          
+         <label  className=" col-sm-2 col-form-label">{t(`levelHours.term`)}</label>
+         <div className="col-sm-4 ">
+         <select className="form-select " >
+           <option selected>{t(`levelHours.term`)}</option>
+           <option>One</option>
+         </select>         
+        </div>
+
+        
+       
+        </div>
+        <div className="row mb-4">
+          
+          <label  className=" col-sm-2 col-form-label">{t(`levelHours.level`)}</label>
+          <div className="col-sm-4 ">
+          <select className="form-select " >
+            <option selected>{t(`levelHours.level`)}</option>
+            <option>One</option>
+          </select>         
+         </div>
+ 
+         
+        
+         </div>
+        <div className="row mb-4">
+          
+          <label  className=" col-sm-2 col-form-label">{t(`levelHours.min`)}</label>
+          <div className="col-sm-4 ">
+          <select className="form-select " >
+            <option selected>{t(`levelHours.min`)}</option>
+            <option>One</option>
+          </select>         
+         </div>
+ 
+         
+ 
+        
+         </div>
+         <div className="row mb-4">
+          
+          <label  className=" col-sm-2 col-form-label">{t(`levelHours.max`)}</label>
+          <div className="col-sm-4 ">
+          <select className="form-select " >
+            <option selected>{t(`levelHours.max`)}</option>
+            <option>One</option>
+          </select>         
+         </div>
+ 
+         </div>
+         <div className="row mb-4">
+          
+          <label  className=" col-sm-2 col-form-label">{t(`levelHours.max`)}</label>
+          <div className="col-sm-4 ">
+          <select className="form-select " >
+            <option selected>{t(`levelHours.max`)}</option>
+            <option>One</option>
+          </select>         
+         </div>
+ 
+         </div>
+         <div className="row mb-4">
+          
+          <label  className=" col-sm-2 col-form-label">{t(`levelHours.max`)}</label>
+          <div className="col-sm-4 ">
+          <select className="form-select " >
+            <option selected>{t(`levelHours.max`)}</option>
+            <option>One</option>
+          </select>         
+         </div>
+ 
+         </div>
+
+     </form>
+     <span><button type="submit" className={styles.formcard_save }>{t(`common.save`)}</button></span>
+     <span><button type="reset" className={styles.formcard_cancel }>{t(`common.cancel`)}</button></span>
+     </div>
+       <div className={styles.tablecard}>
+        <h3>{t(`levelHours.tabletitle`)}</h3>
       <Table
+      
         headerItems={[
-          { id: 1, title: "hello" },
-          { id: 2, title: "byeee" },
-          { id: 3, title: "min" },
-          { id: 4, title: "max" },
+          { id: 1, title: t(`levelHours.term`)},
+          { id: 2, title: t(`levelHours.level`) },
+          { id: 3, title: t(`levelHours.min`) },
+          { id: 4, title: t(`levelHours.max`) },
         ]}
         rowItems={[
-          { id: 1, title: "el sefe", level: 300, minHours: 30, maxHours: 60 },
+          { id: 1, title: "summer", level: 300, minHours: 30, maxHours: 60 },
           { id: 2, title: "first", level: 400, minHours: 20, maxHours: 90 },
-          { id: 3, title: "el sefe", level: 700, minHours: 10, maxHours: 80 },
-          { id: 4, title: "second", level: 600, minHours: 30, maxHours: 70 },
+          { id: 3, title: "summer", level: 200, minHours: 10, maxHours: 80 },
+          { id: 4, title: "second", level: 500, minHours: 30, maxHours: 70 },
         ]}
         editableItems={true}
         deletableItems={true}
       />
+      </div>
     </div>
   );
 };

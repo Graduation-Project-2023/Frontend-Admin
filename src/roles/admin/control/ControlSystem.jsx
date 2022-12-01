b87eeb6df7c5a7c02140d9433b92d7aa460bf2da
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2022-12-01 15:29:26 +0200

    popup

--- src/roles/admin/control/ControlSystem.jsx
+++ src/roles/admin/control/ControlSystem.jsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { useTranslation } from "react-i18next";
+import styles from "./ControlSysytem.module.scss";
+import {RiCloseCircleFill} from "react-icons/ri"
 
 export const ControlSystem = () => {
-  return <div>System</div>;
+  const { t } = useTranslation();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+    <button  onClick={handleShow}>
+    click meee
+    </button>
+
+    <Modal show={show} onHide={handleClose} className={styles.popup}>
+    <Modal.Header className={styles.popup_header}>
+      
+      <Modal.Title className={styles.popup_title}>{t(`academicMain.faculty`)}</Modal.Title> 
+      <button onClick={handleClose}>X</button>
+          <input
+            type="text"
+            placeholder={t("academicMain.search")}
+          />
+
+
+      
+    </Modal.Header>
+    <Modal.Body>
+    
+
+        <div className={styles.popup_list}>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+          <li>كلية الهندسة</li>
+        </div>
+    </Modal.Body>
+    </Modal>
+    </>
+  
+
+
+
+
+
+  )
 };

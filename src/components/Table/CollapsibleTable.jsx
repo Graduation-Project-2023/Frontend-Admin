7fa3f0343c3f4c24eccda2cd8cd607bfa7758f52
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2022-12-06 00:22:28 +0200

    collapse table component

--- src/components/Table/CollapsibleTable.jsx
+++ src/components/Table/CollapsibleTable.jsx
@@ -1,9 +1,74 @@
 import React from "react";
+import { FormCard } from "../FormCard";
+import { useTranslation } from "react-i18next";
+import Accordion from 'react-bootstrap/Accordion';
+
 
 export const CollapsibleTable = (props) => {
+  const headerItems = props.headerItems;
+  const rowItems = props.rowItems;
+  const collapseLevel=props.collapseLevel
+
   return (
-    <div>
-      <table></table>
+    <>
+    <FormCard cardTitle={"helllllllo"}>
+    <form>
+       < div className="row mb-4">
+    <label className="col-sm-2 col-form-label">{"testttt"}</label>
+    <div className="col-sm-5">
+    <input/>
+      </div>
     </div>
+    
+      <button
+          type="submit"
+          className="form-card-button form-card-button-save"
+        >
+          `common.save`
+        </button>
+      
+      </form>
+    </FormCard>
+
+    <Accordion defaultActiveKey={['0']} alwaysOpen className="collapseTable">
+      {collapseLevel.map((item) => {
+                return (
+      <Accordion.Item eventKey={item.id} key={item.id}>
+        <Accordion.Header>{item.arabicName} {item.level}</Accordion.Header>
+        <Accordion.Body> 
+        
+      <form className="collapsetable">
+        <table className="table">
+          <thead>
+            <tr>
+            {headerItems.map((item) => {
+                return (
+                  <th key={item.id} className="collapsetable-header">
+                    {item.title}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+          {rowItems.map((item) => {
+              return (
+            <tr key={item.id} >
+            <td className="collapsetable-items">{item.code}</td>
+            <td className="collapsetable-items">{item.englishName}</td>
+            <td className="collapsetable-items">{item.arabicName}</td>
+            </tr>
+              );
+          })}
+          </tbody>
+          </table>
+            </form>
+            
+        </Accordion.Body>
+        </Accordion.Item>
+                );
+              })}
+      </Accordion>
+  </>
   );
 };

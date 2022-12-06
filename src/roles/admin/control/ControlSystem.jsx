aba023a492da1be3f6f9b78e0d1f5ece33aeee5b
stevensamir20 <steven.samir@eng.suez.edu.eg>
2022-12-06 20:54:59 +0200

    control

--- src/roles/admin/control/ControlSystem.jsx
+++ src/roles/admin/control/ControlSystem.jsx
@@ -1,63 +1,85 @@
-import React from 'react'
 import { useTranslation } from "react-i18next";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const ControlSystem = () => {
   const { t } = useTranslation();
-  
-  const [values,setValues]=useState({classwork:"",midterm:"",final:"",maxGrade:""})
-  const [classwork,setClasswork]=useState('')
-  const [midterm,setMidterm]=useState('')
-  const [final,setFinal]=useState('')
-  const [maxGrade,setMaxGrade]=useState('')
-  const onChange=(e)=>{
-      let name=e.target.name;
-      let value=e.target.value;
-      const newValues = {
-      ...values,
-      [name]: value
-  } 
-  setValues(newValues)
-  calcMax(newValues) 
-  }
-  const calcMax = (newValues) => {
-  const { classwork,midterm,final} = newValues;
-  const newMax = parseInt(classwork,10)+parseInt(midterm,10)+parseInt(final,10)
-  setMaxGrade(newMax)
-  } 
-  
-  
-  return ( 
-  <form>
-         <div className="row mb-4">
-            <label className="col-sm-1 col-form-label">
-              {t("courses.class")}
-            </label>
-            <div className="col-sm-2">
-          <input onChange={onChange} defaultValue={classwork} name='classwork' id="classwork" type="number"/>
-          </div>
-  
-          <label className="col-sm-2 col-form-label">
-              {t("courses.mid")}
-            </label>
-            <div className="col-sm-2">
-          <input onChange={onChange} defaultValue={midterm} name="midterm"  id="midterm" type="number"/>
-          </div>
-  
-          <label className="col-sm-2 col-form-label">
-              {t("courses.final")}
-            </label>
-            <div className="col-sm-2">
-          <input onChange={onChange} defaultValue={final} id="final" name="final" type="number"/>
-          </div>
-  
-  
-         </div>
-         <div className="row mb-4">
-            <label className="col-sm-1 col-form-label">{t("grades.max")}</label>
-            <div className="col-sm-2">
-            <input  disabled onChange={onChange} defaultValue={maxGrade} id="maxGrade" name="maxGrade" type="number"/>
-            </div>
-            </div>
-      </form> );
-  }
+  const classWorkRef = useRef();
+  const midtermRef = useRef();
+  const finalRef = useRef();
+  const maxGrade = 100;
+
+  const [wrongCourseGrades, setWrongCourseGrades] = useState({
+    error: false,
+    errorMessage: "",
+  });
+
+  const handleCourseGrades = () => {
+    if (
+      +classWorkRef.current.value +
+        +midtermRef.current.value +
+        +finalRef.current.value >
+      maxGrade
+    ) {
+      setWrongCourseGrades({
+        error: true,
+        errorMessage: "sum of grades must be equal to the max grade",
+      });
+      classWorkRef.current.value = "";
+      midtermRef.current.value = "";
+      finalRef.current.value = "";
+    } else if (
+      +classWorkRef.current.value +
+        +midtermRef.current.value +
+        +finalRef.current.value ===
+      maxGrade
+    ) {
+      setWrongCourseGrades({ error: false });
+    }
+  };
+
+  return (
+    <form>
+      <div className="row mb-4">
+        <label className="col-sm-1 col-form-label">{t("courses.class")}</label>
+        <div className="col-sm-2">
+          <input
+            onChange={handleCourseGrades}
+            name="classwork"
+            id="classwork"
+            type="number"
+            ref={classWorkRef}
+          />
+        </div>
+
+        <label className="col-sm-2 col-form-label">{t("courses.mid")}</label>
+        <div className="col-sm-2">
+          <input
+            onChange={handleCourseGrades}
+            name="midterm"
+            id="midterm"
+            type="number"
+            ref={midtermRef}
+          />
+        </div>
+
+        <label className="col-sm-2 col-form-label">{t("courses.final")}</label>
+        <div className="col-sm-2">
+          <input
+            onChange={handleCourseGrades}
+            id="final"
+            name="final"
+            type="number"
+            ref={finalRef}
+          />
+        </div>
+      </div>
+      {wrongCourseGrades.error && <div>{wrongCourseGrades.errorMessage}</div>}
+      <div className="row mb-4">
+        <label className="col-sm-1 col-form-label">{t("grades.max")}</label>
+        <div className="col-sm-2">
+          <input disabled value={maxGrade} type="number" />
+        </div>
+      </div>
+    </form>
+  );
+};

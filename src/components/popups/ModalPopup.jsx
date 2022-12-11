e45a5f9ef16d9fbba654be147656f895c242cec6
stevensamir20 <steven.samir@eng.suez.edu.eg>
2022-12-12 00:22:18 +0200

    modal

--- src/components/popups/ModalPopup.jsx
+++ src/components/popups/ModalPopup.jsx
@@ -1,3 +1,73 @@
-export const ModalPopup = () => {
-  return <div>ModalPopup</div>;
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import cookies from "js-cookie";
+import Modal from "react-bootstrap/Modal";
+
+export const ModalPopup = (props) => {
+  const listData = props.popupList.data;
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const authContext = useAuth();
+  const { t } = useTranslation();
+  const currentLanguageCode = cookies.get("i18next") || "en";
+
+  useEffect(() => {
+    setFilteredData(props.popupList.data);
+  }, [props.popupList.data]);
+
+  useEffect(() => {
+    setFilteredData(
+      listData.filter(
+        (item) =>
+          item.englishName?.toLowerCase().includes(searchValue.toLowerCase()) ||
+          item.arabicName?.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    );
+    // eslint-disable-next-line
+  }, [searchValue]);
+
+  const hideModal = () => {
+    props.closeModal();
+  };
+
+  return (
+    <Modal show={true} onHide={hideModal} className="popup">
+      <Modal.Header className="popup_header">
+        <Modal.Title className="popup_title">{t(props.title)}</Modal.Title>
+        <button onClick={hideModal}>X</button>
+        {props.searchable && (
+          <input
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            value={searchValue}
+            placeholder={t("common.search")}
+          />
+        )}
+      </Modal.Header>
+      <Modal.Body>
+        {props.popupList?.state && (
+          <div className="popup_list">
+            {filteredData.map((item) => {
+              return (
+                <Link
+                  key={item.id}
+                  onClick={() => {
+                    authContext.changeCollege(item);
+                  }}
+                  to={props.popupList.path}
+                >
+                  {currentLanguageCode === "en"
+                    ? item.englishName
+                    : item.arabicName}
+                </Link>
+              );
+            })}
+          </div>
+        )}
+        {props.form?.state && <>{props.children}</>}
+      </Modal.Body>
+    </Modal>
+  );
 };
--- src/roles/admin/AdminPortal.jsx
+++ src/roles/admin/AdminPortal.jsx
@@ -1,81 +1,45 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { useAuth } from "../../hooks/useAuth";
 import { BASE_URL } from "../../shared/API";
 import axios from "axios";
-import cookies from "js-cookie";
-import { Popup } from "../../components/popups/Popup";
+import { ModalPopup } from "../../components/popups/ModalPopup";
+import { useTranslation } from "react-i18next";
 
 export const AdminPortal = () => {
-  const { t } = useTranslation();
   const [colleges, setColleges] = useState([]);
-  const [filteredColleges, setFilteredColleges] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  // eslint-disable-next-line
-  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   // eslint-disable-next-line
   const [error, setError] = useState();
-  const authContext = useAuth();
-  const currentLanguageCode = cookies.get("i18next") || "en";
+  const { t } = useTranslation();
 
   useEffect(() => {
-    setLoading(true);
     axios
       .get(BASE_URL + "/colleges")
       .then((res) => {
         setColleges(res.data);
-        setFilteredColleges(res.data);
-        setLoading(false);
       })
       .catch((error) => {
-        setLoading(false);
         console.log(error);
       });
     // eslint-disable-next-line
   }, []);
 
-  useEffect(() => {
-    if (currentLanguageCode === "en") {
-      setFilteredColleges(
-        colleges.filter((item) =>
-          item.englishName.toLowerCase().includes(searchValue.toLowerCase())
-        )
-      );
-    } else {
-      setFilteredColleges(
-        colleges.filter((item) =>
-          item.arabicName.toLowerCase().includes(searchValue.toLowerCase())
-        )
-      );
-    }
-    // eslint-disable-next-line
-  }, [searchValue]);
-
   return (
-    <Popup
-      button={"اظهار قائمة الكليات"}
-      className={"btn"}
-      title={"academicMain.faculty"}
-      searchItem={true}
-    >
-      <div className="popup_list">
-        {filteredColleges.map((item) => {
-          return (
-            <Link
-              key={item.id}
-              onClick={() => {
-                authContext.changeCollege(item);
-              }}
-              to="academic_programs"
-            >
-              {currentLanguageCode === "en"
-                ? item.englishName
-                : item.arabicName}
-            </Link>
-          );
-        })}
+    <>
+      <div
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        {t("اظهار قائمة الكليات")}
       </div>
-    </Popup>
+      {showModal && (
+        <ModalPopup
+          title={"academicMain.faculty"}
+          searchable={true}
+          popupList={{ state: true, data: colleges, path: "academic_programs" }}
+          closeModal={() => {setShowModal(false)}}
+        />
+      )}
+    </>
   );
 };

94b06e704992690c016d6f9bdb2351b9c40de3b8
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2022-12-29 23:30:31 +0200

    student data & UX

--- src/roles/admin/courses/CoursesPortal.jsx
+++ src/roles/admin/courses/CoursesPortal.jsx
@@ -13,6 +13,12 @@ import { FormInput } from "../../../components/forms/FormInput";
 
 export const CoursesPortal = () => {
   const [courseData, setCourseData] = useState([]);
+  const [userUX, setUserUX] = useState({
+    loading: false,
+    delete: false,
+    error: false,
+    error_msg: "",
+  });
   const { t } = useTranslation();
   const { courseCode } = useParams();
   const navigate = useNavigate();
@@ -21,14 +27,22 @@ export const CoursesPortal = () => {
   useEffect(() => {
     if (courseCode !== "add" && courseCode !== undefined) {
       // GET request to get college course by it's id
+      setUserUX((prev) => ({ ...prev, loading: true }));
       axios
         .get(BASE_URL + `/courses/${courseCode}`)
         .then((res) => {
           console.log(res);
           setCourseData(res.data);
+          setUserUX((prev) => ({ ...prev, loading: false }));
         })
         .catch((error) => {
           console.log(error);
+          setUserUX((prev) => ({
+            ...prev,
+            loading: false,
+            error: true,
+            error_msg: error,
+          }));
         });
     } else {
       setCourseData([]);
@@ -55,6 +69,7 @@ export const CoursesPortal = () => {
       id: courseData.id.replace(/\s/g, ""),
       collegeId: authContext.college.id,
     };
+    setUserUX((prev) => ({ ...prev, loading: true }));
     // Condition to check whether it's adding a new course or updating the current
     courseCode !== "add" && courseCode !== undefined
       ? // PUT request to update the current college course
@@ -63,32 +78,54 @@ export const CoursesPortal = () => {
           .then((res) => {
             setCourseData(res.data);
             navigate("/admin_portal/courses");
+            setUserUX((prev) => ({ ...prev, loading: false }));
           })
           .catch((error) => {
             console.log(error);
+            setUserUX((prev) => ({
+              ...prev,
+              loading: false,
+              error: true,
+              error_msg: error,
+            }));
           })
       : // POST request to create a new college course
         axios
           .post(BASE_URL + `/courses`, newCourse)
           .then((res) => {
             console.log(res);
+            setUserUX((prev) => ({ ...prev, loading: false }));
           })
           .catch((error) => {
             console.log(error);
+            setUserUX((prev) => ({
+              ...prev,
+              loading: false,
+              error: true,
+              error_msg: error,
+            }));
           });
   };
 
   const handleCourseDelete = (e) => {
     e.preventDefault();
     // DELETE request to delete the current college course
+    setUserUX((prev) => ({ ...prev, delete: true }));
     axios
       .delete(BASE_URL + `/courses/${courseData.id}`)
       .then((res) => {
         console.log(res);
         navigate("/admin_portal/courses");
+        setUserUX((prev) => ({ ...prev, delete: false }));
       })
       .catch((error) => {
         console.log(error);
+        setUserUX((prev) => ({
+          ...prev,
+          delete: false,
+          error: true,
+          error_msg: error,
+        }));
       });
   };
 
@@ -100,51 +137,62 @@ export const CoursesPortal = () => {
             handleFormSubmit(event);
           }}
         >
-          {CoursesFormData.map((data) => {
-            if (
-              data.name === "id" &&
-              courseCode !== "add" &&
-              courseCode !== undefined
-            ) {
-              return (
-                <FormInput
-                  inputData={{ ...data, disabled: true }}
-                  handleEditFormChange={handleEditFormChange}
-                  valueData={courseData}
-                  key={data.id}
-                />
-              );
-            } else {
-              return (
-                <FormInput
-                  inputData={data}
-                  handleEditFormChange={handleEditFormChange}
-                  valueData={courseData}
-                  key={data.id}
-                />
-              );
-            }
-          })}
+          {userUX.loading ? (
+            "loading..."
+          ) : (
+            <>
+              {CoursesFormData.map((data) => {
+                if (
+                  data.name === "id" &&
+                  courseCode !== "add" &&
+                  courseCode !== undefined
+                ) {
+                  return (
+                    <FormInput
+                      inputData={{ ...data, disabled: true }}
+                      handleEditFormChange={handleEditFormChange}
+                      valueData={courseData}
+                      key={data.id}
+                    />
+                  );
+                } else {
+                  return (
+                    <FormInput
+                      inputData={data}
+                      handleEditFormChange={handleEditFormChange}
+                      valueData={courseData}
+                      key={data.id}
+                    />
+                  );
+                }
+              })}
+            </>
+          )}
+
           <button
             type="submit"
             className="form-card-button form-card-button-save"
           >
-            {courseCode !== "add" && courseCode !== undefined
+            {userUX.loading
+              ? "loading..."
+              : courseCode !== "add" && courseCode !== undefined
               ? t(`common.save`)
               : t(`common.add`)}
           </button>
           <button
             type="reset"
             className="form-card-button form-card-button-cancel"
+            disabled={userUX.loading}
           >
             {t(`common.cancel`)}
           </button>
+
           {courseCode !== "add" && courseCode !== undefined && (
             <button
               className="form-card-button form-card-button-delete"
               onClick={handleCourseDelete}
             >
-              {t(`common.delete`)}
+              {userUX.delete ? "loading..." : t(`common.delete`)}
             </button>
           )}
         </form>

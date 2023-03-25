b302ec283f8d1b0534e4e0efe4e56c94adea6043
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2023-03-25 13:38:51 +0200

    Student Register

---
+++ src/roles/admin/registeration/RegisterationFormData.js
@@ -0,0 +1,56 @@
+export const RegisterationFormData = [
+  {
+    id: 0,
+    splitRow: [
+      {
+        id: 1,
+        title: "common.ar_name",
+        name: "arabicName",
+        type: "text",
+        req: true,
+      },
+      {
+        id: 2,
+        title: "common.eng_name",
+        name: "englishName",
+        type: "text",
+      },
+    ],
+  },
+  {
+    id: 1,
+    splitRow: [
+      {
+        id: 1,
+        title: "common.schoolSeat",
+        name: "seatId",
+        type: "text",
+        req: true,
+      },
+      {
+        id: 2,
+        title: "levels.level",
+        name: "level",
+        type: "text",
+      },
+    ],
+  },
+  {
+    id: 2,
+    splitRow: [
+      //   {
+      //     id: 1,
+      //     title: "academicMain.c_gpa",
+      //     name: "gpa",
+      //     type: "text",
+      //   },
+      {
+        id: 2,
+        title: "academicMain.credit",
+        name: "creditHrs",
+        type: "text",
+        req: true,
+      },
+    ],
+  },
+];

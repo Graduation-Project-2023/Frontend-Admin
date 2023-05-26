a53e518ca32f9695df29b24e13ec693b2fd903f6
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-05-26 21:45:07 +0300

    staff portal

--- src/roles/staff/portal/StaffPortal.jsx
+++ src/roles/staff/portal/StaffPortal.jsx
@@ -1,44 +1,85 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import React, {useState ,useEffect } from "react";
+
+// Components
+import { FaBookReader, FaAddressBook, FaTable, FaBook } from "react-icons/fa";
 import arab from "./arab.png";
 import eng from "./eng.png";
-import { FaBookReader ,FaAddressBook ,FaTable ,FaBook} from "react-icons/fa";
 
 export const StaffPortal = () => {
-  const { t } = useTranslation();
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
 
-  let arabStyle = {backgroundImage: "url(" + arab + ")"};
-  let engStyle = {backgroundImage: "url(" + eng + ")"};
+  let arabStyle = {
+    backgroundImage: "url(" + arab + ")",
+    backgroundSize: "contain",
+  };
+  let engStyle = {
+    backgroundImage: "url(" + eng + ")",
+    backgroundSize: "contain",
+  };
 
-
-  return <div className="container">
-    <div className="staffcont">
-      <div className="staffcont-upperhalf" style={ i18n.language === "ar" ? arabStyle : engStyle}> 
-        <div className="staffcont-upperhalf-text">
-          <h1>{t(`staff.welcome1`)}</h1>
-           <h2>{t(`staff.welcome2`)}</h2>
-           <h3>{t(`staff.welcome3`)}</h3>
+  return (
+    <div className="container">
+      <div className="staffcont">
+        <div
+          className="staffcont-upperhalf"
+          style={i18n.language === "ar" ? arabStyle : engStyle}
+        >
+          <div className="staffcont-upperhalf-text">
+            <h1>{t(`staff.welcome1`)}</h1>
+            <h2>{t(`staff.welcome2`)}</h2>
+            <h3>{t(`staff.welcome3`)}</h3>
+          </div>
         </div>
-      </div>
-      <div className="staffcont-lowerhalf">
-        <div className="staffcont-lowerhalf-card">
-            <span><FaAddressBook style={{fontSize: '2vw'}}/></span>
+        <div className="staffcont-lowerhalf">
+          <div
+            className="staffcont-lowerhalf-card"
+            onClick={() => {
+              navigate("students_info");
+            }}
+          >
+            <span>
+              <FaAddressBook style={{ fontSize: "2vw" }} />
+            </span>
             <h5>{t(`staff.transcript`)}</h5>
-        </div>
-        <div className="staffcont-lowerhalf-card">
-          <span><FaBookReader style={{fontSize: '2vw'}}/></span>
-          <h5>{t(`staff.grades`)}</h5>
-        </div>
-        <div className="staffcont-lowerhalf-card">
-          <span><FaTable style={{fontSize: '2vw'}}/></span>
-          <h5>{t(`staff.schedule`)}</h5>
-        </div>
-        <div className="staffcont-lowerhalf-card">
-          <span>< FaBook style={{fontSize: '2vw'}}/></span>
-          <h5>{t(`staff.bank`)}</h5>  
+          </div>
+          <div
+            className="staffcont-lowerhalf-card"
+            onClick={() => {
+              navigate("coursework");
+            }}
+          >
+            <span>
+              <FaBookReader style={{ fontSize: "2vw" }} />
+            </span>
+            <h5>{t(`staff.grades`)}</h5>
+          </div>
+          <div
+            className="staffcont-lowerhalf-card"
+            onClick={() => {
+              navigate("schedule");
+            }}
+          >
+            <span>
+              <FaTable style={{ fontSize: "2vw" }} />
+            </span>
+            <h5>{t(`staff.schedule`)}</h5>
+          </div>
+          <div
+            className="staffcont-lowerhalf-card"
+            onClick={() => {
+              navigate("mcq");
+            }}
+          >
+            <span>
+              <FaBook style={{ fontSize: "2vw" }} />
+            </span>
+            <h5>{t(`staff.bank`)}</h5>
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };

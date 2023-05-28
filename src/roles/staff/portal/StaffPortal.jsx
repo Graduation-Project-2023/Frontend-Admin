1b3bf84f07b8cb48419197081d683399b720a86b
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-05-29 02:41:10 +0300

    staff portal

--- src/roles/staff/portal/StaffPortal.jsx
+++ src/roles/staff/portal/StaffPortal.jsx
@@ -3,39 +3,32 @@ import { useTranslation } from "react-i18next";
 
 // Components
 import { FaBookReader, FaAddressBook, FaTable, FaBook } from "react-icons/fa";
-import img1 from "./img1.png"
-import img2 from "./img2.png"
-// import arab from "./arab.png";
-// import eng from "./eng.png";
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+
 
 export const StaffPortal = () => {
-   const { t, i18n } = useTranslation();
-   const navigate = useNavigate();
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
 
-  // let arabStyle = {
-  //   backgroundImage: "url(" + arab + ")",
-  //   backgroundSize: "contain",
-  // };
-  // let engStyle = {
-  //   backgroundImage: "url(" + eng + ")",
-  //   backgroundSize: "contain",
-  // };
-
-  let arabimg = { };
-  let engimg = {transform: "rotateY(180deg)"};
-
-  let arabtext = { };
-  let engtext = {transform: "rotateY(180deg)"};
-
-
+  let arabimg = {};
+  let engimg = { transform: "rotateY(180deg)" };
 
   return (
     <div className="container">
       <div className="staffcont">
         <div className="staffcont-upperhalf">
-          <div className="staffcont-upperhalf-img" >
-            <img src={img1} style={i18n.language === "ar" ? arabimg : engimg}/>
-            <img src={img2} style={i18n.language === "ar" ? arabimg : engimg}/>
+          <div className="staffcont-upperhalf-img">
+            <img
+              src={img1}
+              style={i18n.language === "ar" ? arabimg : engimg}
+              alt="welcome"
+            />
+            <img
+              src={img2}
+              style={i18n.language === "ar" ? arabimg : engimg}
+              alt="welcome"
+            />
           </div>
           <div className="staffcont-upperhalf-text">
             <h1>{t(`staff.welcome1`)}</h1>
@@ -43,17 +36,6 @@ export const StaffPortal = () => {
             <h3>{t(`staff.welcome3`)}</h3>
           </div>
         </div>
-
-        {/* <div
-          className="staffcont-upperhalf"
-          style={i18n.language === "ar" ? arabStyle : engStyle}
-        >
-          <div className="staffcont-upperhalf-text">
-            <h1>{t(`staff.welcome1`)}</h1>
-            <h2>{t(`staff.welcome2`)}</h2>
-            <h3>{t(`staff.welcome3`)}</h3>
-          </div>
-        </div> */}
         <div className="staffcont-lowerhalf">
           <div
             className="staffcont-lowerhalf-card"
